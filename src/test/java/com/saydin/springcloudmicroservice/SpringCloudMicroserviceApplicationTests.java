49590c80b61bc375457b731910f79e2f41bc818a
Semiha Aydın <semiha.aydinn@gmail.com>
2023-02-02 11:47:21 +0300

    Initial commit

---
+++ src/test/java/com/saydin/springcloudmicroservice/SpringCloudMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saydin.springcloudmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
