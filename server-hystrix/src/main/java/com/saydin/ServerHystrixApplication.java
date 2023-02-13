6cdc39b32deb8a3b70c101c9ae6f2a458f606ac9
Semiha Aydın <semiha.aydinn@gmail.com>
2023-02-13 17:10:38 +0300

    Spring cloud gateway added with hystrix

---
+++ server-hystrix/src/main/java/com/saydin/ServerHystrixApplication.java
@@ -0,0 +1,12 @@
+package com.saydin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerHystrixApplication.class,args);
+    }
+}
